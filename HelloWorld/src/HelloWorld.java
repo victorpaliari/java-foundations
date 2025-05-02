d7f213191dfeff29fc148309f17f71717765fb9a
Victor Paliari <victorpaliari@MacBook-Air-de-Victor.local>
2025-05-02 11:39:32 -0300

    Sessao II

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,11 @@
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Linha 1");
+        System.out.println("Linha 2");
+        System.out.println("Linha 3");
+        System.out.println("Linha 4");
+    }
+}
---
+++ ProblemSet2_1/ProblemSet2_1.java
@@ -0,0 +1,19 @@
+public class ProblemSet2_1 {
+    public static void main(String[] args) {
+        //Use 8 print statements to print a smiley face.
+        //Your art will rely on only a single character, besides space, such as X or #.
+        
+        
+        
+        System.out.println("  x x x x  ");
+        System.out.println(" x       x ");
+        System.out.println("x  x    x x");
+        System.out.println("x         x");
+        System.out.println("x   xxx   x");
+        System.out.println(" x       x ");
+        System.out.println("  x x x x  ");
+        
+        
+        
+    }   
+}
